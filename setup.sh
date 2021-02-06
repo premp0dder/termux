aae7d4233f8675e7bfd2f43b4b269a9558c9a9fa
Prem Podder <75319073+prempodder@users.noreply.github.com>
2021-02-07 01:30:27 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 apt-get update && apt-get upgrade -y
 apt-get install wget proot git tar curl -y
-cd ~
+clear
+    cp bash.bashrc $HOME
+    cd /data/data/com.termux/files/usr/etc
+    rm -rf motd
+    rm -rf bash.bashrc
+    cd $HOME
+    mv bash.bashrc /data/data/com.termux/files/usr/etc
+    echo -e "\e[1;91mSuccessfully Installed"
+    echo -e "Restart Termux"
 clear
