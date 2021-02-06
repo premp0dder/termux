e8f85575221af70f8818feb0dc54d8befac6792f
Prem Podder <75319073+prempodder@users.noreply.github.com>
2021-02-07 01:40:12 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 termux-setup-storage
+mkdir $HOME/.termux/ ;echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F7'],['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && logout
 apt-get update && apt-get upgrade -y
 apt-get install wget proot git tar zsh curl -y
 clear
