c3e245d780112f94a1d5a65a9a9178a0cbe046c6
Prem Podder <75319073+prempodder@users.noreply.github.com>
2021-02-07 01:35:12 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,33 @@
+termux-setup-storage
 apt-get update && apt-get upgrade -y
-apt-get install wget proot git tar curl -y
+apt-get install wget proot git tar zsh curl -y
 clear
-    cp bash.bashrc $HOME
-    cd /data/data/com.termux/files/usr/etc
-    rm -rf motd
-    rm -rf bash.bashrc
-    cd $HOME
-    mv bash.bashrc /data/data/com.termux/files/usr/etc
-    echo -e "\e[1;91mSuccessfully Installed"
-    echo -e "Restart Termux"
-clear
+
+#shell
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
+
+mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
+sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+chsh -s zsh
+
+echo "oh-my-zsh install complete!\nChoose your color scheme now~"
+$HOME/.termux/colors.sh
+
+echo "Choose your font now~"
+$HOME/.termux/fonts.sh
+
+echo "Please restart Termux app..."
+
+exit
