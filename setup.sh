69ee45472bc1ab70b0f7fee370a4c1e850918d9d
Prem Podder <75319073+prempodder@users.noreply.github.com>
2021-02-07 18:02:21 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,8 @@
 termux-setup-storage
-mkdir $HOME/.termux/ ;echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F7'],['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && logout
+mkdir $HOME/.termux/ ;echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F7'],['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $HOME/.termux/termux.properties
 apt-get update && apt-get upgrade -y
-apt-get install wget proot git tar zsh curl -y
+apt-get install wget proot git tar curl -y
 clear
-
-#shell
-git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
-
-mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
-cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
-
-git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
-mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
-cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
-sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
-sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
-echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
-echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
-
-chsh -s zsh
-
-echo "oh-my-zsh install complete!\nChoose your color scheme now~"
-$HOME/.termux/colors.sh
-
-echo "Choose your font now~"
-$HOME/.termux/fonts.sh
-
 echo "Please restart Termux app..."
 
 exit
